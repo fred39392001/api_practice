a69562b545e47182e6161982b71fd6579f999f4c
Fred Chen <fred.chen@tpisoftware.com>
2022-04-08 16:31:47 +0800

    feat: Add async function code

--- all.js
+++ all.js
@@ -1,23 +1,51 @@
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
+const submitBtn =document.getElementById('submitBtn');
+const resData = document.getElementById('data');
 
-// const submitBtn = document.getElementById('submitBtn');
-// submitBtn.addEventListener("click", processFormData);
+submitBtn.addEventListener('click', function(){
+  const myFormData = new FormData();
+  const url = 'https://eid-backend.townway.com.tw/accounts/signin';
+  
+  myFormData.append('email', emailInput.value);
+  myFormData.append('password', passwordInput.value);
 
-// function processFormData(){
-//   const emailValue = document.getElementById('emailInput').value;
-//   const passwordValue = document.getElementById('passwordInput').value;
-//   alert(`email:${emailValue}password:${passwordValue}`);
-// };
-
-const resData = document.getElementById('data')
-
-const myFormData = new FormData();
-myFormData.append('email', 'fred39392001@gmail.com');
-myFormData.append('password', 'Born1983Abow0827');
-axios.post('https://eid-backend.townway.com.tw/accounts/signin', myFormData)
-.then(function(res){
-  console.log(res.data);
-  resData.textContent = res.data.token;
-})
-.catch(function(err){
-  console.log(err);
+  if(emailInput.value === '' || passwordInput.value === ''){
+    alert('Email 或密碼欄位不得為空!')
+    return;
+  }
+  // axios 寫法
+  // axios.post('https://eid-backend.townway.com.tw/accounts/signin', myFormData)
+  // .then(function(res){
+  //   console.log(res.data);
+  //   alert(`${res.data.username}您好，您已登入成功！`);
+  //   emailInput.value = '';
+  //   passwordInput.value = '';
+  //   renderData(res);
+  // })
+  // .catch(function(err){
+  //   console.log(err);
+  //   alert('登入失敗，請輸入正確的 Email 或密碼！')
+  // });
+  signIn(url, myFormData);
 });
+
+// async 寫法
+async function signIn(url, data) {
+  try {
+    const res = await axios.post(url, data);
+    console.log(res);
+    alert(`${res.data.username}您好，您已登入成功！`);
+    emailInput.value = '';
+    passwordInput.value = '';
+    renderData(res);
+  } catch (error) {
+    console.error(error);
+    alert('登入失敗，請輸入正確的 Email 或密碼！')
+  }
+};
+
+function renderData(receiveData){
+  const prettyString = JSON.stringify(receiveData.data, null, 2)
+  resData.textContent = prettyString;
+};
